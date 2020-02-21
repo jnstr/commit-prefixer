155b51a3530445d66afb90523e31f7d9c7d59980
Jens Trio <jens.trio@teamleader.eu>
2020-02-21 14:21:47 +0100

    [+] Add prompt for the commit type

--- prefixer.sh
+++ prefixer.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+function getCommitType() {
+    options=(
+        "[+] Adding a file or feature"
+        "[B] Fixing a bug"
+        "[-] Removing code or files"
+        "[M] Moving or renaming files"
+        "[L] Fixing linter errors & warnings"
+        "[R] Refactoring existing code"
+        "[T] Adding or updating tests"
+        "[!] Bad code that needs to be improved"
+    )
+
+    select commitType in "${options[@]}"; do
+        if [ -z "$commitType" ]; then
+            echo $commitType
+        else
+            ## extract the prefix (the part before the first whitespace)
+            echo $commitType | sed 's/\s.*$//'
+        fi
+        break
+    done
+}
+
+echo $(getCommitType)
